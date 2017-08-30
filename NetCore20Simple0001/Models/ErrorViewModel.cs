1ca0c69cd1068838c61adebb7296dbc54d2f0a45
Sandwall Oskar <oskar.sandwall@tieto.com>
2017-08-30 12:10:56 +0200

    Add project files.

---
+++ NetCore20Simple0001/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCore20Simple0001.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
